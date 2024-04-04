bd32af1776bab70cb789fd7c6dbe0af0edd96195
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-04 19:16:10 +0330

    chore: rename general folder to common

--- src/App.vue
+++ src/App.vue
@@ -5,48 +5,47 @@
   <linear-indicator
     v-if="indicatorPercentage"
     class="indicator"
-    :percentage="indicatorPercentage"
-  />
+    :percentage="indicatorPercentage" />
 </template>
 
 <script setup>
-import { onUnmounted, ref } from 'vue';
+  import { onUnmounted, ref } from 'vue';
 
-import { eventBus } from '@/plug-in/event-bus';
+  import { eventBus } from '@/plug-in/event-bus';
 
-import LayoutView from '@/layout/layout-view.vue';
-import LinearIndicator from '@/components/general/indicator/linear-indicator.vue';
+  import LayoutView from '@/layout/layout-view.vue';
+  import LinearIndicator from '@/components/common/indicator/linear-indicator.vue';
 
-const indicatorPercentage = ref(0);
+  const indicatorPercentage = ref(0);
 
-const updateIndicatorPercentage = (percentage) => {
-  indicatorPercentage.value = percentage;
-};
-const beforeEachSubscription = eventBus.subscribeOn('router:beforeEach', () =>
-  updateIndicatorPercentage(30)
-);
+  const updateIndicatorPercentage = (percentage) => {
+    indicatorPercentage.value = percentage;
+  };
+  const beforeEachSubscription = eventBus.subscribeOn('router:beforeEach', () =>
+    updateIndicatorPercentage(30)
+  );
 
-const afterEachSubscription = eventBus.subscribeOn('router:afterEach', () => {
-  updateIndicatorPercentage(100);
-  setTimeout(() => updateIndicatorPercentage(0), 300);
-});
+  const afterEachSubscription = eventBus.subscribeOn('router:afterEach', () => {
+    updateIndicatorPercentage(100);
+    setTimeout(() => updateIndicatorPercentage(0), 300);
+  });
 
-const clearSubscriptions = () => {
-  beforeEachSubscription.unsubscribe();
-  afterEachSubscription.unsubscribe();
-};
-onUnmounted(clearSubscriptions);
+  const clearSubscriptions = () => {
+    beforeEachSubscription.unsubscribe();
+    afterEachSubscription.unsubscribe();
+  };
+  onUnmounted(clearSubscriptions);
 </script>
 
 <style lang="scss">
-// FIXME: remove this style tag
+  // FIXME: remove this style tag
 </style>
 
 <style lang="scss" scoped>
-.indicator {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 9999;
-}
+  .indicator {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+  }
 </style>
