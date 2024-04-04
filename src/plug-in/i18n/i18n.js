a51df3ab2c22f334549a45db6ff605730bf28197
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-04 18:51:46 +0330

    feat: add interpolation support

--- src/plug-in/i18n/i18n.js
+++ src/plug-in/i18n/i18n.js
@@ -1,27 +1,35 @@
 import { dictionaries } from './dictionary';
+import { doubleBracketReplace } from '@/utils/string.js';
 
-//FIXME: move this somewhere more general
-const generateError = (code, query) => {
-  //TODO: use different error base on code
-  throw new Error(`Undefined query "${query}"`);
-};
-
-export const translate = (query, locale) => {
-  console.log(query, locale);
+/**
+ *
+ * @param {string} query - query to find in the dictionary
+ * @param {Object} options
+ * @param {string} options.locale - The locale
+ * @param {Object} options.params - parameters to replace within the translation
+ * @returns {string} - translation
+ */
+export const translate = (query, { params, locale }) => {
   const dictionary = dictionaries[locale];
   if (!dictionary) throw new Error(`Unsupported locale "${locale}"`);
 
   try {
     const queryAsList = query.split('.');
-    const queryTranslation = queryAsList.reduce((dictionaryChunk, queryChunk) => {
-      return dictionaryChunk[queryChunk];
-    }, dictionary);
+    const queryTranslation = queryAsList.reduce(
+      (dictionaryChunk, queryChunk) => {
+        return dictionaryChunk[queryChunk];
+      },
+      dictionary
+    );
 
-    if (!queryTranslation) generateError(undefined, query);
+    if (!queryTranslation) {
+      throw new Error(`Undefined query "${query}"`);
+    }
 
-    return queryTranslation;
+    const translation = doubleBracketReplace(queryTranslation, params);
+    return translation;
   } catch (error) {
-    throw generateError(undefined, query);
+    console.error(error);
   }
 };
 
