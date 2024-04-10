12b0239f6fc375921aa0d6b79b7c43a00cc1f6cf
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-10 14:34:25 +0330

    feat: implement after each middlewares

---
+++ src/router/middlewares/after-each.js
@@ -0,0 +1,32 @@
+import { eventBus } from '@/modules/event-bus';
+
+/**
+ * Update document title based on `route.meta.title`
+ * @param {Object} to - VueRouter `to` route object
+ * @returns {undefined}
+ */
+const updateDocumentTitle = (to) => {
+  const title = to.meta.title;
+  if (!title) return;
+
+  document.title = title;
+};
+
+/**
+ * Publish afterEach happened event
+ */
+const publishEvent = () => {
+  eventBus.publish('router:afterEach');
+};
+
+/**
+ * afterEach middlewares call
+ * @param {Object} to - VueRouter `to` route object
+ * @param {Object} from - VueRouter `from` route object
+ */
+const afterEach = (to, from) => {
+  updateDocumentTitle(to, from);
+  publishEvent(to, from);
+};
+
+export default afterEach;
