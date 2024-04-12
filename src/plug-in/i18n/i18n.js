8e17564494dbe297186b269b7a1522acfa1b3741
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-12 18:07:20 +0330

    chore: add double bracket replacer as a utility

--- src/plug-in/i18n/i18n.js
+++ src/plug-in/i18n/i18n.js
@@ -1,5 +1,5 @@
 import { dictionaries } from './dictionary';
-import { doubleBracketReplace } from '@/utils/string.js';
+import { doubleBracketReplace } from '@/utils/double-bracket-replace.js';
 
 /**
  *
