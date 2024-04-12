c51d8e71e74bf4c129b40fece37703fd5576b597
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-12 21:29:47 +0330

    chore: rename file to translate

---
+++ src/plug-in/i18n/translate.js
@@ -0,0 +1,36 @@
+import { dictionaries } from './dictionary/index.js';
+import { doubleBracketReplace } from './utils/double-bracket-replace.util.js';
+
+/**
+ *
+ * @param {string} query - query to find in the dictionary
+ * @param {Object} options
+ * @param {string} options.locale - The locale
+ * @param {Object} options.params - parameters to replace within the translation
+ * @returns {string} - translation
+ */
+export const translate = (query, { params, locale }) => {
+  const dictionary = dictionaries[locale];
+  if (!dictionary) throw new Error(`Unsupported locale "${locale}"`);
+
+  try {
+    const queryAsList = query.split('.');
+    const queryTranslation = queryAsList.reduce(
+      (dictionaryChunk, queryChunk) => {
+        return dictionaryChunk[queryChunk];
+      },
+      dictionary
+    );
+
+    if (!queryTranslation) {
+      throw new Error(`Undefined query "${query}"`);
+    }
+
+    const translation = doubleBracketReplace(queryTranslation, params);
+    return translation;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default { translate };
