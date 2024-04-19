b3245bec2977425b05cb825f65fdf865037eda1f
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-19 20:00:16 +0330

    chore: add mask constants

---
+++ src/constants/mask.constant.js
@@ -0,0 +1,11 @@
+export const REGEX_STARTING_CHAR = '^';
+export const REGEX_ENDING_CHAR = '$';
+export const REGEX_TOKEN = '[^.]';
+export const DEFAULT_TOKEN = '#';
+
+export default {
+  REGEX_STARTING_CHAR,
+  REGEX_ENDING_CHAR,
+  REGEX_TOKEN,
+  DEFAULT_TOKEN,
+};
