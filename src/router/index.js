afe8d33ffe0505f9263b2d3c6f8e7f88d8a1359b
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-25 23:20:44 +0330

    refactor: constant and router structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import ROUTES from '@/router/routes.js';
+import ROUTES_MAP from '@/constants/router/routes.constant.js';
 import Middlewares from '@/router/middlewares';
+
+const routes = Object.values(ROUTES_MAP).map((routeConfig) => routeConfig);
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: ROUTES,
+  routes,
 });
 
 Middlewares.apply(router);
