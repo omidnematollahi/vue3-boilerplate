72a40bc2ab07cd0eae6ace3f9de7c888e31dc9e7
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-26 23:26:28 +0330

    feat: add presenter view to present stuff like components

---
+++ src/views/presentation/presentation-view.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="presentation">
+    <h1 class="presentation__title">Presentation</h1>
+    <ButtonPresentation class="presentation__buttons" />
+  </div>
+</template>
+
+<script setup>
+  import ButtonPresentation from '@/components/view/presentation/button.component.vue';
+</script>
+
+<style lang="scss" scoped>
+  .presentation {
+    padding: space(8);
+
+    &__title {
+      @include typography(display-small);
+    }
+
+    &__buttons {
+      padding: space(6);
+      background-color: var(--palette-surface-container);
+      border-radius: $radius-4x;
+      margin-top: space(4);
+    }
+  }
+</style>
