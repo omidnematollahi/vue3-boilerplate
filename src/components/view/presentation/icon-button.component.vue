6b03dc51629912abb0a4747c68dec8c6b9d39736
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-27 22:45:16 +0330

    chore: add icon button presenter section

---
+++ src/components/view/presentation/icon-button.component.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="presenter">
+    <h3 class="presenter__title">Icon Button Component Presentation</h3>
+    <!-- Filled Icon Button -->
+    <div class="presenter__container">
+      <icon-button variant="filled" icon-name="close" :model-value="false" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+  .presenter {
+    &__title {
+      @include typography(title-medium);
+    }
+
+    &__container {
+      @include flex();
+      gap: space(4);
+      margin-top: space(6);
+    }
+  }
+</style>
