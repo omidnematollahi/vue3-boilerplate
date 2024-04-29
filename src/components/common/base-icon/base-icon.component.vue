dc6dc5e6c97b0e281a017fbcf9baaf038a8f5a46
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-04-29 17:23:02 +0330

    feat: implement fallback icon type

--- src/components/common/base-icon/base-icon.component.vue
+++ src/components/common/base-icon/base-icon.component.vue
@@ -6,15 +6,43 @@
   import { defineAsyncComponent, computed } from 'vue';
 
   const props = defineProps({
-    iconName: String,
+    iconName: {
+      type: String,
+      required: true,
+    },
+    styleType: {
+      type: String,
+      default: 'outlined',
+      validator(type) {
+        return ['filled', 'outlined'].includes(type);
+      },
+    },
   });
+
+  const getFallbackIconType = () =>
+    props.styleType === 'filled' ? 'outlined' : 'filled';
 
   const icon = computed(() => {
     const iconName = props.iconName;
+    const type = props.styleType;
 
-    return defineAsyncComponent(() =>
-      import(`../../../assets/icons/${iconName}.icon.vue`)
-    );
+    return defineAsyncComponent(async () => {
+      let module;
+      try {
+        module = await import(
+          `../../../assets/icons/${type}/${iconName}.icon.vue`
+        );
+      } catch (error) {
+        const fallbackIconType = getFallbackIconType();
+        console.warn(
+          `using fallback icon style ("${iconName}" icon) -> [${fallbackIconType}]`
+        );
+        module = await import(
+          `../../../assets/icons/${fallbackIconType}/${iconName}.icon.vue`
+        );
+      }
+      return module;
+    });
   });
 </script>
 
