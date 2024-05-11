2f05879092dd9fce923231c7a18cd5a13e03fb09
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-11 22:19:21 +0330

    chore: update playground template

--- src/views/playground/playground-view.vue
+++ src/views/playground/playground-view.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <h1 class="title">THIS IS PLAYGROUND</h1>
-    <button @click="$router.push({ name: 'presentation' })">
-      Presentation
-    </button>
+  <div class="container">
+    <radio-input :is-selected="selected" @click="selected = !selected" />
   </div>
 </template>
 
+<script setup>
+  import { ref } from 'vue';
+
+  const selected = ref(true);
+</script>
+
 <style lang="scss" scoped>
-  .title {
-    @include typography(display-medium);
-    color: var(--palette-primary);
+  .container {
+    margin: 64px auto;
+    border: 4px dashed var(--palette-primary);
+    padding: 24px;
+    border-radius: 24px;
+    width: 600px;
+    height: 400px;
   }
 </style>
