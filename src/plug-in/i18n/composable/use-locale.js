7b542f8328c47ad9dcf0fe474a00934fc954a182
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-17 22:54:28 +0330

    feat: add direction computed to useLocale composable

--- src/plug-in/i18n/composable/use-locale.js
+++ src/plug-in/i18n/composable/use-locale.js
@@ -6,11 +6,13 @@ const locale = ref('');
 const useLocale = () => {
   const getLocale = computed(() => locale.value);
 
+  const direction = computed(() =>
+    rtlLocaleList.includes(locale.value) ? 'rtl' : 'ltr'
+  );
+
   const setLocale = (newLocale) => {
     document.documentElement.setAttribute('lang', newLocale);
-
-    const direction = rtlLocaleList.includes(newLocale) ? 'rtl' : 'ltr';
-    document.documentElement.setAttribute('dir', direction);
+    document.documentElement.setAttribute('dir', direction.value);
 
     if (locale.value === newLocale) return;
 
@@ -20,7 +22,7 @@ const useLocale = () => {
     locale.value = newLocale;
   };
 
-  return { locale: getLocale, setLocale, supportedLocaleList };
+  return { locale: getLocale, setLocale, supportedLocaleList, direction };
 };
 
 export default useLocale;
