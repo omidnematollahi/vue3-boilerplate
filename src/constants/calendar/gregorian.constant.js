7969764b5dc5a13d21b650ce8184dd728af48a24
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-18 20:05:14 +0330

    feat: add gregorian calendar constants including month list and week days list

---
+++ src/constants/calendar/gregorian.constant.js
@@ -0,0 +1,26 @@
+export const MONTH_LIST = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const WEEK_DAYS_LIST = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+
+export default { WEEK_DAYS_LIST, MONTH_LIST };
