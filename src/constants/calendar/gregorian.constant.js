be22ce51a70b942922ce3d4ae4778910cd7cc9f4
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-19 09:55:41 +0330

    fix: move sunday to the beginning of the week

--- src/constants/calendar/gregorian.constant.js
+++ src/constants/calendar/gregorian.constant.js
@@ -13,14 +13,6 @@ export const MONTH_LIST = [
   'December',
 ];
 
-export const WEEK_DAYS_LIST = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday',
-];
+export const WEEK_DAYS_LIST = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 export default { WEEK_DAYS_LIST, MONTH_LIST };
