3e739c17871c52cb340dababb75a175f7ff75412
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-20 17:02:18 +0330

    feat: add year picker to date-picker

--- src/components/common/picker/date/date-picker.component.vue
+++ src/components/common/picker/date/date-picker.component.vue
@@ -8,24 +8,36 @@
     <control-menu
       :label-text="monthYearLabel"
       :button-icon="menuButtonIcon"
+      :show-month-controls="!yearPickerVisibility"
       @click:button="yearPickerVisibility = !yearPickerVisibility"
       @action:monthControl="moveViewDateByOneMonth"
     />
-    <div class="calendar-view">
-      <transition
-        :name="calendarTransitionName"
-        @before-enter="toggleViewDateMoveBlock(true)"
-        @after-leave="toggleViewDateMoveBlock(false)"
-      >
-        <base-calendar
-          class="calendar-view__calendar"
-          :current-date="viewDate"
-          :calendar="calendar"
-          :key="viewDate.month"
-          :selected-start-date="modelValue"
-          @click:date="selectDate"
+    <div class="date-picker__main">
+      <transition name="year-picker">
+        <year-picker
+          v-if="yearPickerVisibility"
+          class="date-picker__year-picker"
+          :year-range="yearRange"
+          :selected-year="viewDate.year"
+          @select:year="selectYear"
         />
       </transition>
+      <div class="calendar-view">
+        <transition
+          :name="calendarTransitionName"
+          @before-enter="toggleViewDateMoveBlock(true)"
+          @after-leave="toggleViewDateMoveBlock(false)"
+        >
+          <base-calendar
+            class="calendar-view__calendar"
+            :current-date="viewDate"
+            :calendar="calendar"
+            :key="viewDate.month"
+            :selected-start-date="modelValue"
+            @click:date="selectDate"
+          />
+        </transition>
+      </div>
     </div>
     <slot name="actions">
       <div class="date-picker__actions">
@@ -46,11 +58,16 @@
   import BaseCalendar from '@/components/common/picker/date/base-calendar.vue';
   import ControlMenu from '@/components/common/picker/date/control-menu.vue';
   import DatepickerHeader from '@/components/common/picker/date/header.vue';
+  import YearPicker from '@/components/common/picker/date/year-picker.vue';
 
   const props = defineProps({
     calendar: {
       type: CalendarInterface,
       required: true,
+    },
+    yearRange: {
+      type: Array,
+      default: [1900, 2100],
     },
     placeholder: {
       type: String,
@@ -147,6 +164,11 @@
     return `${weekDay}, ${monthLabelShort} ${day}`;
   });
 
+  const selectYear = (year) => {
+    viewDate.value.year = year;
+    yearPickerVisibility.value = false;
+  };
+
   const pickDate = () => {
     //NOTE: for now only support Object format but here is where we convert formats later
     emit('action:pickDate', modelValue);
@@ -184,11 +206,33 @@
 
 <style lang="scss" scoped>
   .date-picker {
+    $background: var(--palette-surface-container-high);
     width: 360px;
-    background-color: var(--palette-surface-container-high);
+    background-color: $background;
     border-radius: $radius-7x;
 
     &__header {
+      border-bottom: 1px solid var(--palette-outline-variant);
+    }
+
+    &__main {
+      position: relative;
+      overflow: hidden;
+      @include transition() {
+        transition-property: height;
+      }
+
+      height: v-bind('`${calendarViewHeight}px`');
+    }
+
+    &__year-picker {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: $background;
+      z-index: 1;
       border-bottom: 1px solid var(--palette-outline-variant);
     }
 
@@ -201,13 +245,6 @@
   }
 
   .calendar-view {
-    position: relative;
-    overflow: hidden;
-    @include transition() {
-      transition-property: height;
-    }
-
-    height: v-bind('`${calendarViewHeight}px`');
     @include flex;
 
     &__calendar {
