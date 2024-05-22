27b04f68cafce48aa9e187214e77edeb9b18d0d5
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-22 23:44:25 +0330

    chore: update playground for side-sheet component

--- src/views/playground/playground-view.vue
+++ src/views/playground/playground-view.vue
@@ -1,14 +1,165 @@
 <template>
   <div class="container">
-    <date-picker :calendar="gregorianCalendar"></date-picker>
+    <base-button @click="openSideSheet">Open Side Sheet</base-button>
+    <modal-side-sheet
+      v-model="sideSheetVisibility"
+      :has-back-button="true"
+      :has-inset="true"
+      @action:back="sideSheetVisibility = false"
+    >
+      <div class="content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
+          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
+          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
+          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
+          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
+          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
+          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
+          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
+          officia molestias quas molestiae sit.
+        </p>
+      </div>
+    </modal-side-sheet>
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
-  import gregorianCalendar from '@/services/calendar/gregorian';
 
-  const selected = ref(true);
+  const sideSheetVisibility = ref(false);
+
+  const openSideSheet = () => {
+    sideSheetVisibility.value = true;
+  };
 </script>
 
 <style lang="scss" scoped>
@@ -18,5 +169,12 @@
     padding: 24px;
     border-radius: 24px;
     width: 600px;
+  }
+
+  .content {
+    height: 100%;
+    // background: coral;
+    overflow: hidden auto;
+    padding: 0 space(3);
   }
 </style>
