dca7389cd038a6bb43a2c6a743e5197852c05b30
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-23 16:37:25 +0330

    chore: add filter chip playground

--- src/views/playground/playground-view.vue
+++ src/views/playground/playground-view.vue
@@ -1,180 +1,36 @@
 <template>
   <div class="container">
-    <base-button @click="openSideSheet">Open Side Sheet</base-button>
-    <modal-side-sheet
-      v-model="sideSheetVisibility"
-      :has-back-button="true"
-      :has-inset="true"
-      @action:back="sideSheetVisibility = false"
-    >
-      <div class="content">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Expedita voluptas alias fugit assumenda
-          omnis suscipit. Eaque voluptatum veniam suscipit magni earum, magnam
-          nesciunt, fugit ex officia molestias quas molestiae sit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Expedita voluptas alias
-          fugit assumenda omnis suscipit. Eaque voluptatum veniam suscipit magni
-          earum, magnam nesciunt, fugit ex officia molestias quas molestiae sit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          voluptas alias fugit assumenda omnis suscipit. Eaque voluptatum veniam
-          suscipit magni earum, magnam nesciunt, fugit ex officia molestias quas
-          molestiae sit. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Expedita voluptas alias fugit assumenda omnis suscipit. Eaque
-          voluptatum veniam suscipit magni earum, magnam nesciunt, fugit ex
-          officia molestias quas molestiae sit.
-        </p>
-      </div>
-    </modal-side-sheet>
+    <filter-chip
+      class="container__chip"
+      label-text="Filter Chip"
+      leading-icon="favorite"
+      trailing-icon="favorite"
+      :is-selected="isSelected"
+      @click="isSelected = !isSelected"
+    />
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
 
-  const sideSheetVisibility = ref(false);
+  const isSelected = ref(false);
 
   const openSideSheet = () => {
-    sideSheetVisibility.value = true;
+    isSelected.value = true;
   };
 </script>
 
 <style lang="scss" scoped>
   .container {
+    @include flex;
     margin: 64px auto;
     // border: 4px dashed var(--palette-primary);
     padding: 24px;
     border-radius: 24px;
     width: 600px;
-  }
 
-  .content {
-    height: 100%;
-    // background: coral;
-    overflow: hidden auto;
-    padding: 0 space(3);
+    &__chip {
+    }
   }
 </style>
