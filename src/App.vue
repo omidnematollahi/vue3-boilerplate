de8bfe0f630b8797dcbc33be7aad664e4fe859cb
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2024-05-31 17:52:26 +0330

    feat: update app router with transition-router component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <scrim-container>
-    <indicator-router />
+    <indicator-router>
+      <transition-router
+        :route-key="routeKey"
+        default-transition-mode="out-in"
+      />
+    </indicator-router>
   </scrim-container>
   <toast-manager />
 </template>
 
-<script script></script>
+<script setup>
+  import { computed } from 'vue';
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+
+  const routeKey = computed(() => {
+    if (!route.matched.length) return;
+
+    const matchedRoute = route.matched[0];
+    return matchedRoute.name;
+  });
+</script>
